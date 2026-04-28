60d6def955fc282ece4c9ac647e3d3cb80de64a8
bcopauto <mzivkovic@bettercollective.com>
2026-04-28 11:03:56 +0200

    updating settings in orchestrator

--- django_app/orchestrator/urls.py
+++ django_app/orchestrator/urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for orchestrator project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from apps.users.views import root_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.api.urls")),
+    path("", root_view, name="root"),
+    path("accounts/", include("allauth.urls")),
+    path("users/", include("apps.users.urls")),
+    path("api/", include("apps.api.urls")),
 ]
